e629d1bec259120e9da5e5d8e2918bdbe34551db
lwh940824 <lwh940824@gmail.com>
2022-10-19 21:23:40 +0900

    시큐리티 진행중

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,8 +1,10 @@
 package com.cos.blog.service;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,11 +14,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Autowired
     private UserRepository userRepository;
 
     @Transactional
     public void 회원가입(User user) {
-            userRepository.save(user);
+        String rawPassword = user.getPassword();
+        String encPassword = encoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole(RoleType.USER);
+
+        userRepository.save(user);
     }
 
 }
